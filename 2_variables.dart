f37f283ddce86e61fbf0e65826e586fcd416ff45
Adesco Vicdon <105074605+Adescovicdon@users.noreply.github.com>
2022-12-25 19:56:35 +0100

    First commit

---
+++ 2_variables.dart
@@ -0,0 +1,8 @@
+int main (){
+  var name = "Adesco Vicdon";
+  var age = "24";
+  var district = "Akure";
+  
+  print("My name is ${name}, I am ${age} years old, i come from ${district}");
+  return 0;
+}
---
+++ 3_operators.dart
@@ -0,0 +1,24 @@
+int main (){
+/*int x = 2;
+  int y = 3;
+  int z = 0;
+  z = x * y;
+
+  //print(" $x + $y = $z ");
+  //print(" $x - $y = $z ");
+  print(" $x * $y = $z ");*/
+
+  int a = 2;
+  //a++; increase by one
+  //a--; reduce by one
+
+  //a = a + 3; add 3 to a
+  //a += 3; also add 3 to a
+  //a-= 3; minus 3 from a
+
+  int m = 101;
+  int n = m % 3; //the remender if you divide m by 3
+  //int n = m % 100; the remender if you divide n by 100
+  print (n);
+  return 0;
+}
